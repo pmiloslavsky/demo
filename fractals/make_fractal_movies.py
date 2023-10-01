db222aeae5ec72370868c3959a0751909698f18b
pmiloslavsky <philip.miloslavsky@gmail.com>
2023-10-01 15:08:05 -0700

    better parametarization for fractal movies

--- fractals/make_fractal_movies.py
+++ fractals/make_fractal_movies.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import os
 import math
+import sys
 import subprocess
+import argparse
 from ctypes import *
 from tabnanny import filename_only
 from PIL import Image
@@ -81,15 +83,12 @@ def fileio_setup():
     for field_name, field_type in fkey._fields_:
         print(field_name, getattr(fkey, field_name))
 
-def create_evolved_frames(end, seedkey_name):
-    count = end - 1
-    if end > 50:
-        if (end - 1) % 50 != 0:
-            return
+def create_evolved_frames():
+    count = args.tf
 
     currentkey = read_fractal_key(key_location + seedkey_name)
 
-    for j in range(0,end):
+    for j in range(count):
         """Create each png frame of the gif"""
         if j != 0 :
             currentkey = read_fractal_key(changedkey_name)
@@ -97,8 +96,6 @@ def create_evolved_frames(end, seedkey_name):
 
         framekey_name=frame_basename + str(j) + key_version
         f = open(key_location + framekey_name,"wb")
-        #changedkey.current_max_iters0=1500
-        #changedkey.current_escape_r=50
 
         # if you zoom it will change xstart so be careful
 
@@ -106,19 +103,12 @@ def create_evolved_frames(end, seedkey_name):
         #changedkey.xstart = changedkey.xstart + 5*changedkey.xdelta
         
         # zoom
-        if count > 50:
-            currentkey.requested_zoom=currentkey.requested_zoom/(1.0 + (1/50)) #zoom in fast
-        else:
-            currentkey.requested_zoom=currentkey.requested_zoom/(1.0 + (0.1/count)) #zoom in slowly
-        
+        currentkey.requested_zoom=currentkey.requested_zoom/(1.0 + (args.z/count))
 
         # rotate light
-        if end > 50:
-            angle=(end/50)*j*2*math.pi/(count) # several light rotations
-        else:
-            angle=j*2*math.pi/(count) # one light rotation
-
-        #angle=j*2*math.pi/(count) # full circle around full mandelbrot
+        angle=(args.lr)*j*2*math.pi/(count) # several light rotations
+        
+        # light location in mandelbrot
         radius=2
         x = radius * math.cos(angle)
         y = radius * math.sin(angle)
@@ -136,7 +126,7 @@ def create_evolved_frames(end, seedkey_name):
         # this exports the new changedkey into a file
 
 
-def create_gif(end, time):
+def create_gif():
     # Create the frames
     frames = []
     imgs = sorted(glob.glob(png_basename+"*.png"), key=os.path.getmtime)
@@ -151,29 +141,39 @@ def create_gif(end, time):
     frames[0].save(final_basename + ".gif", format='GIF',
                append_images=frames[0:len(imgs)],
                save_all=True,
-               duration=time, loop=0)    
+               duration=args.tg, loop=0)    
 
-def create_movie(end, fps, movie_name):
+def create_movie(fps, movie_name):
     imgs = sorted(glob.glob(png_basename+"*.png"), key=os.path.getmtime)
     clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(imgs, fps=fps)
     clip.write_videofile(movie_name + ".mp4")
 
 def main():
+    global args
+    parser = argparse.ArgumentParser(description="Create gif and 2 movies from fractal evolution")
+    parser.add_argument("lr", type=float, help="total light full cricle rotations")
+    parser.add_argument("z", type=float, help="zoom in z times")
+    parser.add_argument("tf", type=int, help="total frames")
+    parser.add_argument("--tg", type=int, default=20,  help="time in seconds for gif")    
+    args = parser.parse_args()
+    
+    for i in range(len(sys.argv)):
+        print(sys.argv[i])
+        
     os_setup()
     fileio_setup()
+ 
 
     #starts with movie_base.fractal_key_version_1 in demo/fractals directory
     #finishes with 3 output files in fractals_cuda/x64/Release
     # check into top level dir if they look good
     
-    #total frames to go around circle with light
-    total_frames=30
-    create_evolved_frames(total_frames, seedkey_name)
+    create_evolved_frames()
 
-    create_gif(total_frames, 100)
+    create_gif(args.tg)
 
-    create_movie(total_frames, 5, final_basename + "_slow")
-    create_movie(total_frames, 30, final_basename + "_fast")
+    create_movie(5, final_basename + "_slow")
+    create_movie(30, final_basename + "_fast")
 
     imgs = sorted(glob.glob(png_basename+"*.png"), key=os.path.getmtime)
     for i in range(len(imgs)):
